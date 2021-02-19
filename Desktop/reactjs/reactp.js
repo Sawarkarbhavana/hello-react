4b13c7325b7d88dec84acf4328c8834cd0473399
sawarkarbhavana <sawarkarbhavana@gmail.com>
2021-02-19 05:46:17 -0500

    bhareact

---
+++ Desktop/reactjs/reactp.js
@@ -0,0 +1,16 @@
+
+<script src =https://cdnjs.cloudflare.com/ajax/libs/react/16.13.1/umd/react.production.min.js ></script>
+<script src =https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.13.1/umd/react-dom.production.min.js></script>
+
+
+
+function Person() {
+  return (
+    <div className="person">
+   <h1>Bhavana <h1>
+    <p>Your Age: 28</p>
+    </div>
+  );
+}
+     
+ ReactDOM.render(<Person/>,document.querySelector('#p2'));  
