d831a489b953914d453c261935b32528a674d3f1
sawarkarbhavana <sawarkarbhavana@gmail.com>
2021-02-19 05:55:59 -0500

    updates

---
+++ Desktop/REACTJS-CODE/hello-world/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Greet from './Components/greet'
+
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+      <Greet></Greet>
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ Desktop/REACTJS-CODE/hello-world/src/Components/greet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// function Greet() {
+
+// return <h1>Hello Beautiful!! </h1>
+
+// }
+
+const Greet = () => <h1> Hello</h1>
+
+export default Greet
